1faf77d311136b284e8b29313318ba6168de129e
red-apple38 <alibozkoyun38@gmail.com>
2026-06-11 15:31:45 +0200

    refactor: move reusable code into project namespace

--- tests/test_package_imports.py
+++ tests/test_package_imports.py
@@ -3,9 +3,10 @@ from importlib import import_module
 
 def test_installed_packages_are_importable():
     modules = [
-        "beginner.gradient_descent.gradient_descent",
-        "beginner.perceptron.perceptron",
-        "illustration_utils.plot_image",
+        "understand_deep_learning",
+        "understand_deep_learning.beginner.gradient_descent.gradient_descent",
+        "understand_deep_learning.beginner.perceptron.perceptron",
+        "understand_deep_learning.illustration_utils.plot_image",
     ]
 
     for module in modules:
